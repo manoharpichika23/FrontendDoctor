e392fdd96a21eecbe355adc7184366916b48ecac
manoharpichika23 <pichikamanohar23@gmail.com>
2024-03-14 11:41:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Doctor from './Doctor';
+import Patient from './Patient';
+import Home from './Home';
+import Navigate from './Navigate';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+  
+   return(
+    <BrowserRouter>
+      <Navigate/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/doctors" element={<Doctor/>} />
+        <Route path="/patients" element={<Patient/>} />
+      </Routes>
+    </BrowserRouter>
+   )
+  }
 
 export default App;
---
+++ src/Navigate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigate = () => {
+    return (
+        <nav className="navbar">
+           <a id="logo">TRUE <span>CARE +</span></a>
+            <Link to="/">Home</Link>
+
+            <Link to="/doctors">Doctors</Link>
+
+            <Link to="/patients">Patients</Link>
+
+        </nav>
+    );
+};
+
+export default Navigate;
